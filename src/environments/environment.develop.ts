63bb49b2c31154b771f7b3466ef3d1b2f691b61c
Wesley Simplicio <wesleysimplicio@live.com>
2020-05-15 17:14:07 -0300

    add env develop

---
+++ src/environments/environment.develop.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://promotion-fast.herokuapp.com/promotion-management/api/',
+  apiUrlPrice: 'https://price-management-develop.fastshopdev.com/price-management/api/',
+  callBackFastChannel: 'https://fastshop-promotion.herokuapp.com/login',
+  callLogout: 'https://fastshop-promotion.herokuapp.com/login',
+  urlFastChannel: 'https://auth.kaive.com.br',
+  clientFastChannel: 'portal-multiplecar'
+};
