c04408b3cd7d2ef48cf04f7011d382fc1952b0a1
Wesley Simplicio <wesleysimplicio@live.com>
2020-06-01 15:48:03 -0300

    env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://promotion-fast.herokuapp.com/promotion-management/api/',
-  apiUrlPrice: 'https://price-management-develop.fastshopdev.com/price-management/api/',
+  apiUrl: 'https://api.fastshop.com.br/promotion-management/',
+  apiUrlPrice: 'https://api.fastshop.com.br/price-management/',
   callBackFastChannel: 'https://fastshop-promotion.herokuapp.com/login',
   callLogout: 'https://fastshop-promotion.herokuapp.com/login',
   urlFastChannel: 'https://auth.kaive.com.br',
