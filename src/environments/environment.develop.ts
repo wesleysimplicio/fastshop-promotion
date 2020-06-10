494d092a102a8d9dc9307dd5b00348e6eb0b9f3c
Wesley Simplicio <wesleysimplicio@live.com>
2020-06-10 12:03:11 -0300

    ajuste env e build;

--- src/environments/environment.develop.ts
+++ src/environments/environment.develop.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://promotion-fast.herokuapp.com/promotion-management/api/',
-  apiUrlPrice: 'https://price-management-develop.fastshopdev.com/price-management/api/',
+  apiUrlPrice: 'https://price-management-qa.fastshopdev.com/price-management/api/',
   callBackFastChannel: 'https://fastshop-promotion.herokuapp.com/login',
   callLogout: 'https://fastshop-promotion.herokuapp.com/login',
   urlFastChannel: 'https://auth.kaive.com.br',
