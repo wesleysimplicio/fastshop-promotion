1a6faa049de94fce3f6c1fb4af63b53609b5411d
Wesley Simplicio <wesleysimplicio@live.com>
2020-06-10 18:09:14 -0300

    heroku ajuste;

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(`${__dirname}/dist/FastShopPromotion`));
+app.use(express.static(`${__dirname}/dist`));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(`${__dirname}/dist/FastShopPromotion/index.html`));
+    res.sendFile(path.join(`${__dirname}/dist/index.html`));
 });
 
 app.listen(process.env.PORT || 8080);
